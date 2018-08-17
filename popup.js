618e0a48a9d36314de1730c05a92271ac6ee9c9b
TinyKitten <catwalk8205@gmail.com>
2018-08-18 01:38:59 +0900

    動線を少なくした

--- popup.js
+++ popup.js
@@ -1,13 +1,16 @@
 window.onload = () => {
   chrome.tabs.getSelected(window.id, function(tab) {
-    var urlElem = document.getElementById('url');
-    urlElem.innerText = `${tab.title} - ${tab.url}`;
+    var instanceElem = document.getElementById('instance');
+    var textElem = document.getElementById('text');
+    textElem.value = `${tab.title} - ${tab.url}`;
     var btn = document.getElementById('submit');
     btn.onclick = () => {
-      openYam(tab.title, tab.url);
+      if (textElem.value !== '' && instanceElem.value !== '') {
+        openYam(instanceElem.value, tab.title, tab.url);
+      }
     };  
   });
 };
-function openYam(title, url) {
-  chrome.tabs.create({url: `https://yam.tinykitten.me/post?text=${encodeURIComponent(title)} - ${encodeURIComponent(url)}`});
+function openYam(instance, title, url) {
+  chrome.tabs.create({url: `https://yam.tinykitten.me/post?instance=${instance}&text=${encodeURIComponent(title)} - ${encodeURIComponent(url)}`});
 }
