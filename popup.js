ee9b43da24d2528c6b4b53e7ec55f22562b27c6a
TinyKitten <catwalk8205@gmail.com>
2018-08-18 01:45:32 +0900

    直近に使ったインスタンスを記録

--- popup.js
+++ popup.js
@@ -1,12 +1,16 @@
 window.onload = () => {
   chrome.tabs.getSelected(window.id, function(tab) {
     var instanceElem = document.getElementById('instance');
+
+    chrome.storage.local.get('lastInstance', (val) => instanceElem.value = val.lastInstance);
+
     var textElem = document.getElementById('text');
     textElem.value = `${tab.title} - ${tab.url}`;
     var btn = document.getElementById('submit');
     btn.onclick = () => {
       if (textElem.value !== '' && instanceElem.value !== '') {
         openYam(instanceElem.value, tab.title, tab.url);
+        chrome.storage.local.set({'lastInstance': instanceElem.value});
       }
     };  
   });
