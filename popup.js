a02ae526065e472b0b7e5040ab7062992ea00c23
TinyKitten <catwalk8205@gmail.com>
2018-08-17 13:56:00 +0900

    タイトルも突っ込む

--- popup.js
+++ popup.js
@@ -1,20 +1,19 @@
 window.onload = () => {
   chrome.tabs.getSelected(window.id, function(tab) {
-    var url = document.createTextNode(tab.url);
-    document.getElementById('url').appendChild(url);
+    var urlElem = document.getElementById('url');
+    urlElem.innerText = `${tab.title} - ${tab.url}`;
     var btn = document.getElementById('submit');
     btn.onclick = () => {
-      console.log(tab.url);
-      openYam(tab.url);
+      openYam(tab.title, tab.url);
     };  
   });
 };
-function openYam(url) {
+function openYam(title, url) {
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     const tabID = tabs[0].id;
     if (tabID === undefined) {
       return;
     }
-    chrome.tabs.update(tabID, {url: `https://yam.tinykitten.me/post?text=${url}`});
+    chrome.tabs.update(tabID, {url: `https://yam.tinykitten.me/post?text=${title} - ${url}`});
   });
 }
